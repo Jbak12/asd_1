efec13f7a9b31c0c1dd2831112deb58cfbcbab09
Jakub Bakalarz <kubokik5@gmail.com>
2023-11-04 22:56:52 +0100

    removed debug lines and improved countdigit func

--- kolejka/Radix.cpp
+++ kolejka/Radix.cpp
@@ -3,7 +3,8 @@
 #include "LinkedQueue.hpp"
 
 int countDigits(int number) {
-    return (number < 10) ? 1 : 1 + countDigits(number / 10);
+    return floor(log10(number) + 1);  
+
 }
 void radix(std::vector<int>& v) {
     int moduler_divider = 1;
@@ -11,24 +12,20 @@ void radix(std::vector<int>& v) {
     int max_num_of_digits = countDigits(*std::max_element(v.begin(),v.end()));
     LinkedQueue<int>* queues = new LinkedQueue<int>[10];
     for(int i = 0; i < max_num_of_digits; i++) {
+
         for(const int& number : v){
             index_to_push = (number%(moduler_divider*10)-number%moduler_divider)/moduler_divider;
             queues[index_to_push].push(number);
         }
-        for(int i = 0; i< 10; i++) {
-            std::cout<<"kolejka nr "<<i<<": ";
-            (queues[i].printAll());
-        }
 
         moduler_divider = moduler_divider*10;
-        
         v = {};
+
         for(int i = 0; i<10; i++) {
             while(!queues[i].empty()) {
                 v.push_back(queues[i].pop());
             }
         }
-            std::cout<< "i jeszcze raz ; )"<<std::endl;
     }
     delete[] queues;
 }
