fb46a755600ae65fc1fc61887743378206096fd6
Jbak12 <kubokik5@gmail.com>
2023-12-22 16:29:52 +0100

    tree fx

---
+++ ogolne/tester/Zestaw06/BinaryTree.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include "BinaryTree.hpp"
+int main() {
+    BinaryTree tree = BinaryTree();
+}
--- ogolne/tester/Zestaw06/BinaryTree.hpp
+++ ogolne/tester/Zestaw06/BinaryTree.hpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 class BinaryTree {
     public:
     struct Node {
@@ -16,45 +16,64 @@ class BinaryTree {
         if(_size == 0) {
             root = node_to_insert;
         } else {
-            Node* current_node = root;
-            sub_insert(root, node_to_insert);
+            current_depth++;
+            insert(root, node_to_insert);
         }
 
         _size++;
 
     }
 
-    void sub_insert(Node* root, Node* node_to_insert) {
-        if(node_to_insert->val > root->val) {
+    void insert(Node* root, Node* node_to_insert) {
+        if(node_to_insert->val >= root->val) {
             if(root->right == nullptr) {
                 root->right = node_to_insert;
                 return;
             } else {
-                sub_insert(root->right,node_to_insert);
+                insert(root->right,node_to_insert);
             }
-        } else {
+        } else if(node_to_insert->val < root->val)  {
             if(root->left == nullptr) {
                 root->left = node_to_insert;
                 return;
             } else {
-                sub_insert(root->left,node_to_insert);
+                insert(root->left,node_to_insert);
             }
         }
     }
 
     Node* search(int x) {
-
+        auto node = root;
+        while(!(node == nullptr || x == node-> val)) {
+            if(x < node->val) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+        return node;
     }
+
+    
 
     Node* searchRecursive(int x) {
 
     }
 
     int minimum() {
-
+        auto node = root;
+        while(node->left != nullptr) {
+            node = node->left;
+        }
+        return node->val;
     }
 
     int maximum() {
+        auto node = root;
+        while(node->right != nullptr) {
+            node = node->right;
+        }
+        return node->val;
     }
 
     int depth() {
@@ -69,16 +88,39 @@ class BinaryTree {
 
     }
     void inorder() {
+        inorder(root);
+    }
 
+    void postorder(Node* node) {
+        if (node!= nullptr) {
+            inorder(node->left);
+            std::cout<<node->val<<std::endl;
+            inorder(node->right);
+
+        }
+    }
+
+    void preorder(Node* node) {
+        if (node!= nullptr) {
+            inorder(node->left);
+            std::cout<<node->val<<std::endl;
+            inorder(node->right);
+
+        }
+    }
+
+    void inorder(Node* node) {
+        if (node!= nullptr) {
+            inorder(node->left);
+            std::cout<<node->val<<std::endl;
+            inorder(node->right);
+
+        }
     }
 
 
     int size();
 
-
-
-
-    
 
 
     private:
